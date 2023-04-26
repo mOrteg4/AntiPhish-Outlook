7ab59780653c2534fb6afb308e83be6417d9fb87
Justin To <superont@gmail.com>
2023-04-26 15:20:38 -0700

    added instructions

--- run.bat
+++ run.bat
@@ -1,4 +1,12 @@
+rem This batch file installs dependencies and starts the server
+rem Install Python dependencies
 pip install -r requirements.txt
+
+rem Allow network access for WebView control
 CheckNetIsolation LoopbackExempt -a -n="microsoft.win32webviewhost_cw5n1h2txyewy" 
+
+rem Change to application directory by opening the folder and copy the path, then paste it here and remove the " "
 cd C:\Users\super\Documents\GitHub\AntiPhish-Outlook
+
+rem Start the server
 npm run start
